cee6569bb73529a3a5b0cc05164aecbe24616cd5
StephenResch <157391205+StephenResch@users.noreply.github.com>
2026-09-08 11:07:26 -0400

    Created Basic enemy chase movement and damage
    Enemy now chases the player based on their position, though it has a seizure while doing so. Player also gets damaged when the enemy is touching them.

--- tech-games-2627/player.gd
+++ tech-games-2627/player.gd
@@ -30,8 +30,12 @@ func _process(delta):
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
-	print(position)
+	player_position.emit(position)
 	
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_h = velocity.x < 0
+
+
+func _on_basic_enemy_player_hit() -> void:
+	print("hit")
