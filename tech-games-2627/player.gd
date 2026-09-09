703a9f2b3c732b2baa4e35f2886d40ee626a5835
DylanMechler <27DMechler@live.ytech.edu>
2026-09-09 14:10:40 -0400

    PlayerMeleeAttack
    Added Heavy and Light attacks

--- tech-games-2627/player.gd
+++ tech-games-2627/player.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
+signal PlayerLightMeleeAttack
+signal PlayerHeavyMeleeAttack
+
 @export var speed = 200
 @export var damage = 10
 @export var health = 100
+@export var meleeAttackSpeed = 0.75
+var attackReady = true
 var screen_size # Size of the game window
 
 # Called when the node enters the scene tree for the first time.
@@ -24,13 +29,30 @@ func _process(delta):
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+		$PlayerAnimatedSprite.play()
 	else:
-		$AnimatedSprite2D.stop()
+		$PlayerAnimatedSprite.stop()
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
 	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_h = velocity.x < 0
+		$PlayerAnimatedSprite.animation = "walk"
+		$PlayerAnimatedSprite.flip_h = velocity.x < 0
+	
+	if attackReady:
+		if Input.is_action_pressed("light_attack"):
+			PlayerLightMeleeAttack.emit()
+			print("Light Attack")
+			attackReady = false
+			$MeleeAttackTimer.start(meleeAttackSpeed)
+	if attackReady:
+		if Input.is_action_pressed("heavy_attack"):
+			PlayerHeavyMeleeAttack.emit()
+			print("Heavy Attack")
+			attackReady = false
+			$MeleeAttackTimer.start(meleeAttackSpeed)
+
+
+func _on_melee_attack_timer_timeout():
+	attackReady = true
