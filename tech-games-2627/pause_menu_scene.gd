7fc759ffd047dd143e329553f193c15abd6a2344
StephenResch <157391205+StephenResch@users.noreply.github.com>
2026-09-17 14:50:50 -0400

    Broken needs fixing

--- tech-games-2627/pause_menu_scene.gd
+++ tech-games-2627/pause_menu_scene.gd
@@ -1,27 +1,34 @@
 extends CanvasLayer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print("PauseMenuScene _ready() called - Instance ID: ", get_instance_id())
+	hide()
+	get_tree().paused = false
+	# Set to always process in code, so it gets set fresh each time
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
-
+	if Input.is_action_just_pressed("pause_game"):
+		if get_tree().paused:
+			get_tree().paused = false
+			hide()
+		else:
+			get_tree().paused = true
+			show()
+			$VBoxContainer/ResumeButton.grab_focus()
 
 func _on_resume_button_pressed() -> void:
-	pass # Replace with function body.
-
+	print("Resume pressed - Instance: ", get_instance_id())
+	get_tree().paused = false
+	hide()
 
 func _on_save_button_pressed() -> void:
 	print("Save")
 
-
 func _on_settings_button_pressed() -> void:
 	print("Settings")
 
-
 func _on_main_menu_button_pressed() -> void:
-	pass # Replace with function body.
+	print("Main menu pressed - Instance: ", get_instance_id())
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://main_menu.tscn")
