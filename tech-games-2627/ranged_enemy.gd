d03223d6a7ea4d86cc4b1a4acb4867da5f1cb971
DylanMechler <27DMechler@live.ytech.edu>
2026-09-21 14:12:01 -0400

    RangedEnemy

---
+++ tech-games-2627/ranged_enemy.gd
@@ -0,0 +1,65 @@
+extends Area2D
+@onready var roam_timer: Timer = $Timer
+@onready var attack_timer: Timer = $AttackTimer
+signal player_hit(damage)
+var screen_size
+var movement_direction: Vector2 = Vector2.ZERO
+var player_position: Vector2 = Vector2.ZERO
+var player_detect = false
+@export var speed = 150
+@export var damage = 5
+@export var health = 30
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	randomize()
+	roam_timer.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if player_detect:
+		velocity = (player_position - position).normalized() * speed
+	else:
+		velocity = movement_direction * speed
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if health <= 0:
+		queue_free()
+
+
+func choose_new_direction():
+	var random_x = randf_range(-1.0, 1.0)
+	var random_y = randf_range(-1.0, 1.0)
+	movement_direction = Vector2(random_x, random_y).normalized()
+
+
+
+func _on_roam_timer_timeout():
+	if !player_detect:
+		choose_new_direction()
+	roam_timer.wait_time = randf_range(1.5, 4.0)
+
+
+func _on_detection_area_area_entered(_area):
+	player_detect = true
+
+
+func _on_attack_timer_timeout():
+		player_hit.emit(damage)
+
+
+func _on_detection_area_area_exited(_area):
+	player_detect = false
+
+
+func _on_area_entered(_area):
+	attack_timer.start()
+
+
+func _on_area_exited(_area):
+	attack_timer.stop()
